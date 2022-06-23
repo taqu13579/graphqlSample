ad5138a1bc74f1b1b7e0703a8154c6b04b792ba5
taqu13579 <92580230+taqu13579@users.noreply.github.com>
2022-06-23 22:38:00 +0900

    Initial commit

---
+++ src/test/java/com/sample/graphqlSample/GraphqlSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.graphqlSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
